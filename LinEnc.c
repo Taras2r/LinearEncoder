8b2417cd3267d77f72ae0c3188fa4fb501f36dc5
Taras Koverko <E1772434@UAPC1405.emrsn.org>
2024-11-12 23:34:05 +0200

    Calculation of pitch angle versus us, counter value, rod position have been added.

--- LinEnc.c
+++ LinEnc.c
@@ -1,5 +1,8 @@
 
 #include<stdio.h>
+#include<math.h>
+
+
 typedef unsigned char      U8;
 typedef unsigned short     U16;
 typedef unsigned int       U32;
@@ -19,12 +22,14 @@ struct InterpolationData
 {
     S32 position_us;
     S32 positionDeg;
+    S32 positionCount;
+    S32 positionMM;
 };
 static struct InterpolationData linearEncLookUpTable[TABLE_SIZE];
 
 static void initLookUpTable(void)
 {
-   linearEncLookUpTable[0].positionDeg = -1000;
+   linearEncLookUpTable[0].positionDeg = -200;
    linearEncLookUpTable[0].position_us = 2989;
    linearEncLookUpTable[1].positionDeg = 0;
    linearEncLookUpTable[1].position_us = 3221;
@@ -68,52 +73,150 @@ static void initLookUpTable(void)
    linearEncLookUpTable[20].position_us = 20272;
 }
 
-static void interpolateEncoderData( S32 actualPos_us, S32* angleActual,
-                                                    struct InterpolationData *enc )
+#define US 0
+#define COUNTS 1
+#define MM 1
+static void interpolateEncoderData( S32 actualPos,
+                                    S32* angleActual,
+                                    struct InterpolationData *enc,
+                                    U8 us_counts_selector )
 {
     int index = 1;
 
-    if( actualPos_us < enc[0].position_us
-        ||
-        actualPos_us > enc[TABLE_SIZE - 1].position_us )
+    if( US == us_counts_selector )
     {
-        printf( "Linear Enc Out Off Range" );
-        printf( "%i", actualPos_us );
-    }
-    else
-    {
-        for( ; enc[index].position_us < actualPos_us; index++ );
+       if( actualPos < enc[0].position_us
+           ||
+           actualPos > enc[TABLE_SIZE - 1].position_us )
+       {
+           printf( "Linear Enc Out Off Range\r\n" );
+           printf( "Line %i Value: %i\r\n", __LINE__, actualPos );
+       }
+       else
+       {
+           for( ; enc[index].position_us < actualPos; index++ );
 
-        *angleActual =
+           *angleActual =
            (S32)( (S64)(enc[index].positionDeg - enc[index - 1].positionDeg ) *
-                       (S64)( actualPos_us - enc[index - 1].position_us ) /
+                          (S64)( actualPos - enc[index - 1].position_us ) /
                      ( enc[index].position_us - enc[index - 1].position_us ) +
                                                   enc[index - 1].positionDeg );
+       }
+    }
+    else if( COUNTS == us_counts_selector )
+    {
+       if( actualPos < enc[0].positionCount
+           ||
+           actualPos > enc[TABLE_SIZE - 1].positionCount )
+       {
+           printf( "Linear Enc Out Off Range\r\n" );
+           printf( "Line %i Value: %i\r\n", __LINE__, actualPos );
+       }
+       else
+       {
+           for( ; enc[index].positionCount < actualPos; index++ );
+
+           *angleActual =
+           (S32)( (S64)(enc[index].positionDeg - enc[index - 1].positionDeg ) *
+                    (S64)( actualPos - enc[index - 1].positionCount ) /
+                  ( enc[index].positionCount - enc[index - 1].positionCount ) +
+                                               enc[index - 1].positionDeg );
+       }
+    }
+    else if( MM == us_counts_selector )
+    {
+       if( actualPos < enc[0].positionMM
+           ||
+           actualPos > enc[TABLE_SIZE - 1].positionMM )
+       {
+           printf( "Linear Enc Out Off Range\r\n" );
+           printf( "Line %i Value: %i\r\n", __LINE__, actualPos );
+       }
+       else
+       {
+           for( ; enc[index].positionMM < actualPos; index++ );
+
+           *angleActual =
+           (S32)( (S64)(enc[index].positionDeg - enc[index - 1].positionDeg ) *
+                    (S64)( actualPos - enc[index - 1].positionMM ) /
+                  ( enc[index].positionMM - enc[index - 1].positionMM ) +
+                                               enc[index - 1].positionDeg );
+       }
     }
 
-//    printf( "actualPos %i; Index %i", actualPos_us, index );
+//    printf( "actualPos %i; Index %i", actualPos, index );
 //    printf( "maxPos %i; minPos %i", enc[TABLE_SIZE - 1].position_us,
 //                                                          enc[0].position_us );
 }
+
+static void countToMM(S32 actualCounts, U16 entrie)
+{
+   static const U32 P_HubLinearEncInMinBlade = 1435;
+   static const U32 P_HubLinearEncOutMaxBlade = 47500;
+   static const U32 P_HubLinearEncOutMinBlade = 0;
+   static const U32 P_HubLinearEncInMaxBlade = 9731;
+   static const U32 P_HubLinearEncOffsetBlade = 0;
+
+   linearEncLookUpTable[entrie].positionMM =
+         ( S32 )( ( U64 ) ( ( U64 ) actualCounts - P_HubLinearEncInMinBlade )
+      * ( ( U64 ) P_HubLinearEncOutMaxBlade - P_HubLinearEncOutMinBlade )
+      / ( U64 ) ( P_HubLinearEncInMaxBlade - P_HubLinearEncInMinBlade )
+      + P_HubLinearEncOutMinBlade ) + P_HubLinearEncOffsetBlade;
+}
+//resolution at 48MHz 0.0572mm
 int main(void)
 {
    FILE *fileDesc;
    fileDesc = fopen("linEnc.csv", "w");
 
-   fprintf(fileDesc, "period,angle\n");
+   fprintf(fileDesc, "period,counts,angle_us,angleCounts,angleMM,MM\n");
    initLookUpTable();
    S32 actualPos_us = linearEncLookUpTable[0].position_us;
-   static S32 angleCalc;
-   static S32 *angleActual = &angleCalc;
+   S32 actualPosMM = linearEncLookUpTable[0].positionMM;
 
-   while( actualPos_us < linearEncLookUpTable[TABLE_SIZE - 1].position_us )
+   static S32 angleCalcUs;
+   static S32 angleCalcCounts;
+   static S32 angleCalcMM;
+
+   U32 cnt = 0;
+   static const U8 countsPerUs = 48;
+   while( TABLE_SIZE > cnt )
    {
-      interpolateEncoderData( actualPos_us, angleActual, linearEncLookUpTable );
+      linearEncLookUpTable[cnt].positionCount =
+      (S32)(round((F32)linearEncLookUpTable[cnt].position_us
+                     / 100.0 * countsPerUs));
 
-      fprintf( fileDesc, "%i,%i\n", actualPos_us, angleCalc);
-      actualPos_us += 1;
+      countToMM( linearEncLookUpTable[cnt].positionCount, cnt );
+      printf("%i\t",linearEncLookUpTable[cnt].positionCount);
+      printf("%i\n",linearEncLookUpTable[cnt].positionMM);
+      cnt++;
    }
+   static S32 actualPosCounts;
+   actualPosCounts = linearEncLookUpTable[0].positionCount;
+//   printf("%i\n",linearEncLookUpTable[TABLE_SIZE - 1].positionCount);
 
+   while( linearEncLookUpTable[TABLE_SIZE - 1].positionCount >= actualPosCounts )
+   {
+      interpolateEncoderData( actualPos_us, &angleCalcUs,
+                              linearEncLookUpTable, US );
+      interpolateEncoderData( actualPosCounts, &angleCalcCounts,
+                              linearEncLookUpTable, COUNTS );
+      interpolateEncoderData( actualPosCounts, &angleCalcMM,
+                              linearEncLookUpTable, MM );
+
+      fprintf( fileDesc, "%i,%i,%i,%i,%i,%i\n", actualPos_us, actualPosCounts,
+               angleCalcUs, angleCalcCounts, angleCalcMM, actualPosMM );
+
+      actualPosCounts += 1;
+      //actualPos_us scale 100
+      actualPos_us = (S32)round( (F32)100.0 * actualPosCounts
+                                                      / (F32)countsPerUs );
+      //actualPosMM scale 100
+      actualPosMM = (S32)round(
+                  (F32)(actualPosCounts - linearEncLookUpTable[0].positionCount)
+                                 * 5.725650916104146576663452266152 );
+   }
+/**/
    fclose(fileDesc);
    return 0;
 }
